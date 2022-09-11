da6b79c2c83a290efefe9ea0d763b11963b147a2
octopuscart <octopuscartltd@gmail.com>
2022-09-12 02:04:42 +0530

    changes

--- application/config/configuration_db.php
+++ application/config/configuration_db.php
@@ -1,6 +1,6 @@
 <?php
-$activedb = "j2k5e6r5_rahman";
-//$activedb = "j2k5e6r5_royaltailor";
+//$activedb = "j2k5e6r5_rahman";
+$activedb = "j2k5e6r5_royaltailor";
 $activedbusername = "j2k5e6r5_octopus";
 $activedbpassword = "India$2017";
 ?>
